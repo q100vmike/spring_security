86d5fe07beb38b31bf28e0a13edf1087daa0f1eb
q100vmike <melkin@mail.ru>
2024-07-05 16:44:31 +0300

    commit1

---
+++ src/main/java/ru/mtshomework/spring_security/config/AppSecurityConfig.java
@@ -0,0 +1,27 @@
+package ru.mtshomework.spring_security.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+@Configuration
+public class AppSecurityConfig {
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        InMemoryUserDetailsManager inMemoryUserDetailsManager = new InMemoryUserDetailsManager();
+        inMemoryUserDetailsManager
+                .createUser(User.withUsername("Mike")
+                .password("pass").roles("USER").build());
+        return inMemoryUserDetailsManager;
+    }
+
+    public PasswordEncoder passwordEncoder() {
+        return NoOpPasswordEncoder.getInstance();
+    }
+
+}
---
+++ src/main/java/ru/mtshomework/spring_security/controller/AppController.java
@@ -0,0 +1,13 @@
+package ru.mtshomework.spring_security.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AppController {
+
+    @GetMapping("/hello")
+    public String getHello() {
+        return "Hello World from Spring Security";
+    }
+}
